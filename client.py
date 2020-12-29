2d1ab71464e2378097eeb7bfe3a54653463640db
thezohandvir <dror.tomer.il@gmail.com>
2020-12-29 17:29:23 +0200

    TCP after UDP not working

--- client.py
+++ client.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from sys import *
-#from server import openTCPConnection
-from socket import *
-
-
-pavelIP = '192.168.1.104'
-tomerIP = '192.168.14.6'
-=======
->>>>>>> b40772276b60f8853abe809c7f9a30c1e2c34414
 
 from socket import *
 
@@ -28,21 +18,8 @@ def pyUDPClient():
     clientsocket.connect(addr)
     clientsocket.send('i need you i want you')
     response = clientSocket.recv(1024)
-<<<<<<< HEAD
-    print (response)
-    # serverPort=2010
-    # serverName = pavelIP #the IP you want to connect to
-    # clientSocket = socket(AF_INET,SOCK_DGRAM)
-    # while 1:
-    #     message, serverAddress = clientSocket.recvfrom(1024)
-        # print(message)
-
-    # message = b"HEY Tomer its ron and we connect server to client"
-    # clientSocket.sendto(message,(serverName,serverPort))
-=======
 
     print("The time got from the server is %s" % response.decode('ascii'))
->>>>>>> b40772276b60f8853abe809c7f9a30c1e2c34414
 
 
 def main():
--- server.py
+++ server.py
@@ -5,60 +5,8 @@ from struct import *
 from time import *
 
 
-<<<<<<< HEAD
-# def pyUDPServer():
-#     print( "begin the server" )
-#     serverPort = 2010
-#     severSocket = socket(AF_INET,SOCK_DGRAM)
-# # Enable broadcasting mode
-#     severSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-#     # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#     severSocket.bind(('',serverPort))
-#     message = pack('IBH', 4276993775, 2, 2010)
-#     print(message)
-#     print(unpack('IBH',message))
-#     print ('Server started, listening on IP address 172.1.0.4')
-#     print( "The server is ready to receive" )
-#     while 1:
-#         severSocket.sendto( message , ('255.255.255.255', 2010))
-#         sleep(1)
-#         # print( "wait for message" )
-#         # messsage, clientAddress = severSocket.recvfrom(1024)
-#         # timeout(1)
-#         # print(unpack('IBH',message))
-#         # # print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
-#         # # modifiedMessage = messsage.lower() # a func on the message
-#         # # print (modifiedMessage)
-#         # severSocket.sendto(b'good',clientAddress)
-#         # pass
-
-# def pyThreadedTCPServer():
-#     print( "begin the server" )
-#     serverPort = 2010
-#     severSocket = socket(AF_INET,SOCK_DGRAM)
-#     severSocket.bind(('',serverPort))
-#     print( "The server is ready to receive" )
-#     while 1:
-#         print( "wait for UDP message" )
-#         messsage, clientAddress = severSocket.recvfrom(2010)
-#         print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
-#         if ( str(messsage, 'utf-8') == "UDP 1" ):
-#             severSocket.sendto(messsage.upper(),clientAddress)
-#             openTCPConnection( clientAddress )
-#         # modifiedMessage = messsage.upper() # a func on the message
-#         #print (modifiedMessage)
-#         #serverSocket.sendto(modifiedMessage,clientAddress)
-#         pass
-
-# server.py 
-
-
-
-def pyUDPServer2():
-=======
 def pyUDPServer():
 
->>>>>>> b40772276b60f8853abe809c7f9a30c1e2c34414
     # create a socket object
     serversocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
     # get local machine name
@@ -73,17 +21,9 @@ def pyUDPServer():
     serversocket.settimeout(0.2)
     message = b"your very important message"
     # for i in range(11):
-        serversocket.sendto(message, ('<broadcast>', 9999))
-        print("message sent!")
-        sleep(1)
-<<<<<<< HEAD
-    # queue up to 5 requests
-    serversocket.listen(3)                                           
-    while True:
-        # establish a connection
-        clientsocket,addr = serversocket.accept()      
-=======
-    
+    serversocket.sendto(message, ('<broadcast>', 9999))
+    print("message sent!")
+    sleep(1)
     TCPserverSocket = socket(AF_INET, SOCK_STREAM)
     TCPserverSocket.bind(('',port))
 
@@ -93,8 +33,6 @@ def pyUDPServer():
     while True:
         # establish a connection
         clientsocket,addr = TCPserverSocket.accept()      
-
->>>>>>> b40772276b60f8853abe809c7f9a30c1e2c34414
         print("Got a connection from %s" % str(addr))
         currentTime = ctime(time()) + "\r\n"
         clientsocket.send(currentTime.encode('ascii'))
