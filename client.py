3b2c05cbe7ccb1efb53817328b9bd35befee52b6
Armaj <chizback@gmail.com>
2020-12-29 13:27:24 +0200

    asd

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 from socket import *
 
+pavelIP = '192.168.1.104'
+
 def connect_to_server():
     print("in client function")
     serverPort=2010
-    serverName= '192.168.14.6' #the IP you want to connect to
+    serverName = pavelIP #the IP you want to connect to
     clientSocket = socket(AF_INET,SOCK_DGRAM)
     message = b"HEY Tomer its ron and we connect server to client"
     clientSocket.sendto(message,(serverName,serverPort))
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from socket import *
+from _thread import *
+import threading
 
 def pyUDPServer():
     print( "begin the server" )
@@ -10,7 +12,22 @@ def pyUDPServer():
         print( "wait for message" )
         messsage, clientAddress = severSocket.recvfrom(2010)
         print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
-        modifiedMessage = messsage.upper() # a func on the message
+        modifiedMessage = messsage.lower() # a func on the message
+        print (modifiedMessage)
+        severSocket.sendto(modifiedMessage,clientAddress)
+        pass
+
+def pyThreadedTCPServer():
+    print( "begin the server" )
+    serverPort = 2010
+    severSocket = socket(AF_INET,SOCK_DGRAM)
+    severSocket.bind(('',serverPort))
+    print( "The server is ready to receive" )
+    while 1:
+        print( "wait for message" )
+        messsage, clientAddress = severSocket.recvfrom(2010)
+        print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
+        modifiedMessage = messsage.lower() # a func on the message
         print (modifiedMessage)
         severSocket.sendto(modifiedMessage,clientAddress)
         pass
