aab846efbbdaa95c4c103780f84195d98dff7d95
Armaj <chizback@gmail.com>
2020-12-29 15:22:57 +0200

    test

--- client.py
+++ client.py
@@ -4,13 +4,19 @@ pavelIP = '192.168.1.104'
 
 def connect_to_server():
     print("in client function")
-    serverPort=2010
-    serverName = pavelIP #the IP you want to connect to
+
+
+    # serverPort=2010
+    # serverName = pavelIP #the IP you want to connect to
     clientSocket = socket(AF_INET,SOCK_DGRAM)
-    message = b"HEY Tomer its ron and we connect server to client"
-    clientSocket.sendto(message,(serverName,serverPort))
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2055)
-    print(modifiedMessage)
+    clientSocket.bind(('',2010))
+
+    message, serverAddress = clientSocket.recvfrom(1024)
+    print(message)
+
+    # message = b"HEY Tomer its ron and we connect server to client"
+    # clientSocket.sendto(message,(serverName,serverPort))
+    
     clientSocket.close()
 
 
--- server.py
+++ server.py
@@ -1,20 +1,30 @@
 from socket import *
 from _thread import *
 import threading
+from struct import *
 
 def pyUDPServer():
     print( "begin the server" )
     serverPort = 2010
     severSocket = socket(AF_INET,SOCK_DGRAM)
+# Enable broadcasting mode
+    severSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     severSocket.bind(('',serverPort))
+    message = pack('IBH', 4276993775, 2, 2010)
+    print(message)
+    print(unpack('IBH',message))
+    print ('Server started, listening on IP address 172.1.0.4')
+    severSocket.sendto( message , ('255.255.255.255', 2010))
     print( "The server is ready to receive" )
     while 1:
         print( "wait for message" )
-        messsage, clientAddress = severSocket.recvfrom(2010)
-        print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
-        modifiedMessage = messsage.lower() # a func on the message
-        print (modifiedMessage)
-        severSocket.sendto(modifiedMessage,clientAddress)
+        messsage, clientAddress = severSocket.recvfrom(1024)
+        timeout(1)
+        print(unpack('IBH',message))
+        # print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
+        # modifiedMessage = messsage.lower() # a func on the message
+        # print (modifiedMessage)
+        severSocket.sendto(b'good',clientAddress)
         pass
 
 def pyThreadedTCPServer():
