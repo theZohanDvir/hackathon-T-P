3ce60a014f556d615192fbb859bb3e7859347b0f
Armaj <chizback@gmail.com>
2020-12-29 17:46:21 +0200

    yes

--- client.py
+++ client.py
@@ -5,31 +5,23 @@ from socket import *
 def pyUDPClient():
     print("in client function")
     s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-
     s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
     # get local machine name
     host = gethostname()                           
-
-    port = 9999
-
+    print (host)                       
+    port = 2010
     # connection to hostname on the port.        
-
-    s.bind(('', port))
-    
+    s.bind((host, port))
     data, addr = s.recvfrom(1024)
     print("received message: %s"%data)  
-    
     s.close()                  
-
+    # TCP connection   
     clientsocket = socket(AF_INET, SOCK_STREAM)
     clientsocket.connect(addr)
     clientsocket.send('i need you i want you')
-
     response = clientSocket.recv(1024)
-
-    print("The time got from the server is %s" % response.decode('ascii'))
-
+    print("The message is {}".format(response))
+    clientsocket.close()
 
 def main():
     print("client begin")
--- server.py
+++ server.py
@@ -9,18 +9,11 @@ def pyUDPServer():
 
     # create a socket object
     serversocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-
     # get local machine name
     host = gethostname()                           
-
     port = 9999                                           
-
-    # # bind to the port
-    # serversocket.bind(('', port))                                  
-
 # Enable broadcasting mode
     serversocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
     # Set a timeout so the socket does not block
     # indefinitely when trying to receive data.
     serversocket.settimeout(0.2)
@@ -29,20 +22,19 @@ def pyUDPServer():
         serversocket.sendto(message, ('<broadcast>', 9999))
         print("message sent!")
         sleep(1)
-    
+    serversocket.close()
     TCPserverSocket = socket(AF_INET, SOCK_STREAM)
     TCPserverSocket.bind(('',port))
-
     # queue up to 5 requests
     TCPserverSocket.listen(3)                                           
 
     while True:
         # establish a connection
         clientsocket,addr = TCPserverSocket.accept()      
-
         print("Got a connection from %s" % str(addr))
-        currentTime = ctime(time()) + "\r\n"
-        clientsocket.send(currentTime.encode('ascii'))
+        clientsocket.send(b'congragulations')
+
+    clientsocket.close()
 
 def main():
     print("Hello World! this is the server!!")
